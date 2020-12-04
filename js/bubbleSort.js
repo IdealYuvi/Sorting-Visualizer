e92477d738165177ace31830b6d285fc866b9ff2
Yuvaraj <69077336+IdealYuvi@users.noreply.github.com>
2020-12-04 19:38:53 +0530

    Completed Project

---
+++ js/bubbleSort.js
@@ -0,0 +1,60 @@
+class BubbleSort {
+  constructor(bubbleSortButton) {
+    this.bubbleSortButton = bubbleSortButton;
+
+    this.bubbleSortButton.addEventListener('click', this.sort);
+  }
+
+  sort = async () => {
+    let array = document.querySelectorAll('.block');
+    let isSorted = false;
+    let counter = 0;
+
+    while (!isSorted) {
+      isSorted = true;
+
+      for (let i = 0; i < array.length - 1 - counter; i++) {
+        let firstElement = Number(array[i].textContent);
+        let secondElement = Number(array[i + 1].textContent);
+        array[i].style.backgroundColor = '#ec524b';
+        array[i + 1].style.backgroundColor = '#ec524b';
+
+        await new Promise((resolve) =>
+          setTimeout(() => {
+            resolve();
+          }, 400)
+        );
+
+        if (firstElement > secondElement) {
+          array[i].style.backgroundColor = '#13CE66';
+          array[i + 1].style.backgroundColor = '#13CE66';
+
+          await this.swap(i, array);
+
+          isSorted = false;
+        }
+
+        array[i].style.backgroundColor = '#ffa62b';
+        array[i + 1].style.backgroundColor = '#ffa62b';
+      }
+      counter += 1;
+    }
+  };
+
+  swap = (i, array) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        let tempHeight = array[i + 1].style.height;
+        let tempContent = array[i + 1].textContent;
+
+        array[i + 1].style.height = array[i].style.height;
+        array[i + 1].textContent = array[i].textContent;
+
+        array[i].style.height = tempHeight;
+        array[i].textContent = tempContent;
+
+        resolve();
+      }, 400);
+    });
+  };
+}
